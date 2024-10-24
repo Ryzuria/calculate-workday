161e6cbe3dc46c8c8571824647a3fc39f9ab368c
Chonnakan Chinayothin <chonnakan@odds.team>
2024-10-24 09:29:04 +0700

    fix: counting total day

--- workday.ts
+++ workday.ts
@@ -42,7 +42,7 @@ function countSatSun(startDate: Date, endDate: Date) {
 
 function numberOfDays(startDate: Date, endDate: Date) {
   const diffTime = endDate.getTime() - startDate.getTime();
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
   return diffDays + 1;
 }
 
